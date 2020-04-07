253f3775c432893a7eb3ffbbd71007efccfe0e2b
rom1te <romcm16@gmail.com>
2020-04-07 22:37:12 +0200

    ajout ctrl exo_20

--- exo_20.rb
+++ exo_20.rb
@@ -1,13 +1,15 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 nbr_floors = gets.to_i
+if nbr_floors >1 && nbr_floors < 25
+	puts "Voici la pyramide :"
 
-puts "Voici la pyramide :"
-
-nbr_floors.times do |i|
-        print "#"
-        i.times do
-                print "#"
+	nbr_floors.times do |i|
+        	print "#"
+        	i.times do
+                	print "#"
+		end
+		puts " "
 	end
-	puts " "
+else
+	puts "Plus de 1 et moins de 25 stp"
 end
-
